0c797c5565d31eee1da2922c6f4d80cae03fe9ba
matthijs.waag <matthijs.waag@d5577902-0f3f-11df-8aad-07c239688bdb>
2010-11-04 15:50:52 +0000

    rewrote contours, added pitch method

--- python/lib/SCClientRef.py
+++ python/lib/SCClientRef.py
@@ -23,7 +23,7 @@ class SCClient( object ):
 
         try:
             
-            server = scsynth.connect(verbose=True)
+            server = scsynth.connect(verbose=True,iphost="10.0.135.15")
             self._scServer = server
 
             #register some handler
@@ -58,43 +58,73 @@ class SCClient( object ):
     #hue  -> pitch (moet conceptueel)
     #size -> noise_ratio , groter is ook harder
     #xpos -> pan logisch
+    
+    def getPitch( self, contour ):
+        x,y,amp,hue,id = contour
+        
+        
+        ranges = [(0,10),(10, 20),(20, 40),(40, 90),(90, 120),(120, 160),(160,180)]        
+        range = [r for r in ranges if r[0] <= hue and hue <= r[1]]
+        
+        
+        p = ranges.index(range[0])
+
+        oct = [1.0, 9.0/8, 5.0/4, 4.0/3, 3.0/2, 5.0/3, 15.0/8]
+        pitch = oct[p] * 200;
+    
+        if amp < 0.1:
+            pitch = pitch * 2# oct[p] * 400
+            
+        return pitch;
 
     def onNew( self, contour ):
         if not self._scServer:
             return;
 
-        id, amp, pan, hue, sat, val = contour["oid"], contour["size"], contour["x"], contour["hue"], contour["sat"], contour["val"];
-        pitch = hue * 100 + 100
-        # if amp < 0.1:
-        # 	        pitch = pitch * 2 #octave
+        x,y,amp,hue,id = contour
+        
+        
+        #ranges = [(0,10),(10, 30),(30, 60),(60, 90),(90, 120),(120, 160),(160,180)]        
+        #range = [r for r in ranges if r[0] <= hue and hue <= r[1]]
+        #
+        #
+        #p = ranges.index(range[0])
+        #
+        #oct = [1.0, 9.0/8, 5.0/4, 4.0/3, 3.0/2, 5.0/3, 15.0/8]
+        #pitch = oct[p] * 200;
+        #
+        #if amp < 0.1:
+        #    pitch = pitch * 2# oct[p] * 400
+        
+        pitch = self.getPitch( contour )
                 
+        #pitch = hue * 100 + 100
         amp = 0.1 + (amp * 0.8) #minimal amp needed
-
-
-        #msg = ['/s_new', self._synthName, id, 0, self._gid, 'hue', hue, 'amp', amp, 'pan', pan, 'sat', sat, 'val', val];
-        msg = ['/s_new', 'pluckbow', id, 0 , 0, "drive_in", 0, "noise_ratio",amp,"driver_smoothing", 0.995, "damping", 0.95, "refl", 0.999, "pitch_in", pitch, "pan", pan ]
+        msg = ['/s_new', 'pluckbow', id, 0 , 0, "drive_in", 0, "noise_ratio",amp,"driver_smoothing", 0.995, "damping", 0.95, "refl", 0.999, "pitch_in", pitch, "pan", x ]
         self._scServer.sendMsg(*msg)
 
     def onChanged( self, contour ):
         if not self._scServer:
             return
-
-			
-        id, amp, pan, hue, sat, val = contour["oid"], contour["size"], contour["x"], contour["hue"], contour["sat"], contour["val"];
-        damping = 0.9 - (contour["y"] / 10)
+        
+        x,y,amp,hue,id = contour
+        
+        damping = 0.9 - (y/10)
         refl = 0.999# - (contour["y"] / 100)
         amp = 0.1 + (amp * 0.8)
         #refl = 0.9 + (contour["y"] / 10)
 
-        msg = ['/n_set', id, 'drive_in', pan, "damping",damping,"noise_ratio",amp,"pan", pan,"refl",refl] #,"refl",0.9 + amp/10];
+        pitch = self.getPitch( contour )
+
+        msg = ['/n_set', id, 'drive_in', x, "damping",damping,"noise_ratio",amp,"pan", x,"refl",refl, 'pitch', pitch] #,"refl",0.9 + amp/10];
 
         self._scServer.sendMsg(*msg)
 
-    def onLost( self, contour ):
+    def onLost( self, cid ):
         #print "lost"
         #print contour["oid"]
         #print contour;
-        cid = contour["oid"]
+        #cid = conto
         self._lost.append(cid)
         if not self._scServer:
             return;
